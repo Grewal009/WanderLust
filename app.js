f835d2a5224dff44a74ae10caa2a273097b9c2e8
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-24 23:00:59 +0100

    PATCH /listings/:id update listing

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 8080;
 
 const path = require("path");
+const methodOverride = require("method-override");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 const Listing = require("./models/listing");
 
 const mongoose = require("mongoose");
@@ -46,6 +48,29 @@ app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing
     .save()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/listings");
+});
+
+// edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  console.log(listing);
+
+  res.render("edit.ejs", { listing });
+});
+
+// update listing
+app.patch("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const updateListing = req.body.listing;
+  await Listing.findByIdAndUpdate(id, updateListing)
     .then((res) => {
       console.log(res);
     })
